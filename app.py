8ccea913c4a866c9f8850eb3886e5fa1e464609b
Declan <starpdec+GITHUB@gmail.com>
2020-11-20 15:49:50 -0500

    track user ids + rooms also make emotion endpoint more general

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, request
 from datetime import datetime
 import re
+
+# Calls to external services
 from spotify import search
 from azure_cognitive import emotion
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello, Flask!"
+rooms = []
+user_ids = {}
 
-@app.route("/azure/cognitive/emotion")
-def get_emotion():
-    return emotion()
+@app.route("/join/<room_code>", methods=['POST'])
+def user_join(room_code):
+    uid = request.form.get('userId')
+    rooms.append(room_code)
+    user_ids[uid] = room_code
+    return "You're in."
+
+@app.route("/emotion", methods=['POST'])
+def determine_emotion():
+    return emotion(request.form.get('imageUrl'))
 
 # Spotify calls
 @app.route("/search/<query>")
