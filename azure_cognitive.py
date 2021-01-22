952c0295fb7509ea1d0f4bc478f3a05ad0611a8e
Declan <starpdec+GITHUB@gmail.com>
2021-01-22 15:05:49 -0500

    /emotion now return only emotion data + method to handle stream input image

--- azure_cognitive.py
+++ azure_cognitive.py
@@ -1,6 +1,12 @@
+import base64
+import glob
 import json
+from io import BytesIO
+
 import requests
+from azure.cognitiveservices.vision.face import FaceClient
 from flask import jsonify
+from msrest.authentication import CognitiveServicesCredentials
 
 # Azure properties
 SUBSCRIPTION_KEY = 'a7f97fe3646d49dea6a12ede3c1c7804'
@@ -16,4 +22,24 @@ def emotion(image_url):
         'returnFaceId': 'true'
     }
     response = requests.post(FACE_URL, params=params, headers=headers, json={"url": image_url}, verify=False)
-    return jsonify(response.json())
+    json_response = extract_json(response.text)
+    j = json.loads(json_response)
+    return j["faceAttributes"]
+
+
+def extract_json(data):
+    return data[1:-1]
+
+# Will be used to handle image stream from front-end
+def emotion_with_stream(test):
+    print(test)
+    with open('test-url.txt', 'r') as file:
+        data = file.read()
+
+    with open('test.png', 'wb') as file:
+        file.write(base64.decodebytes(data))
+    print(type(data))
+    tt = data.encode('utf-8')
+    face = face_client.face.detect_with_stream(tt, detectionModel='detection_02')
+    print(face)
+
