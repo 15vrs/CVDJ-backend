f0fbf77ef28b8f9e4c77f5003582c5a2b7e5b100
Declan <starpdec+GITHUB@gmail.com>
2021-03-16 11:28:44 -0400

    remove trailing slash

--- services/azure_cognitive.py
+++ services/azure_cognitive.py
@@ -8,7 +8,7 @@ import database.users as users
 
 # Azure properties
 SUBSCRIPTION_KEY = 'bc67fef798e4492a9cff3ee6d0000b19'
-RESOURCE_ENDPOINT = 'https://cvdj-face.cognitiveservices.azure.com/'
+RESOURCE_ENDPOINT = 'https://cvdj-face.cognitiveservices.azure.com'
 FACE_URL = RESOURCE_ENDPOINT + '/face/v1.0/detect'
 
 face_client = FaceClient(RESOURCE_ENDPOINT, CognitiveServicesCredentials(SUBSCRIPTION_KEY))
